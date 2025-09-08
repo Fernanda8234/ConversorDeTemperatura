b4fa8ae2b2d1da6286d85975b2026fc3a7c761c3
FERNANDA CARDOSO DA MOTA <25203628@CFP127ALUNO.LOCAL>
2025-09-08 11:09:28 -0300

    projeto finalizado

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o programa...");
+        System.out.println("Dados");
+        Temperatura temperatura = new Temperatura();
+        temperatura.exibirResultado();
+    }
+}
---
+++ src/Temperatura.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Temperatura {
+    double celsius;
+    double kelvin;
+    double fahrenheit;
+
+    public void solicitarDados(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor em Celsius:" );
+        celsius = scanner.nextDouble();
+    }
+    public void efetuarCalculo(){
+        solicitarDados();
+        Scanner scanner = new Scanner(System.in);
+        fahrenheit = (celsius * 1.8) + 32;
+        kelvin = celsius + 273.15;
+
+    }
+    public void exibirResultado(){
+        efetuarCalculo();
+        System.out.println("---------------------");
+        System.out.println("  R E S U L T A D O  ");
+        System.out.println(celsius + "°C equivalem a:");
+        System.out.println(fahrenheit + "°F (Fahrenheit)");
+        System.out.println(kelvin + "K (Kelvin)");
+        System.out.println("----------------------");
+    }
+}
